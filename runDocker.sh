d7cc6fdd0d0e618ee666ca3698d1bdc7e7a27a09
hillwater <hillwater.zju@gmail.com>
2023-10-16 17:59:51 +0800

    add run docker script

---
+++ runDocker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+HOST=172.17.0.1
+docker run -d  -e CLOUDAMQP_URL=amqp://hillwater:hillwater@$HOST:5672/gomoku -e REDIS_HOST=$HOST  -p 80:3000 --name gomoku-web2 hillwater/gomoku-web:1.1.0
