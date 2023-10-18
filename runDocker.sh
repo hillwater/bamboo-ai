7d6773123dee066cc1bcc8f134f596d9e083c094
hillwater <hillwater.zju@gmail.com>
2023-10-18 09:41:55 +0800

    add stop logic in runDocker

--- runDocker.sh
+++ runDocker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+docker stop gomoku-web2
+docker rm gomoku-web2
+
 HOST=172.17.0.1
 docker run -d  -e CLOUDAMQP_URL=amqp://hillwater:hillwater@$HOST:5672/gomoku -e REDIS_HOST=$HOST  -p 80:3000 --name gomoku-web2 hillwater/gomoku-web:1.1.0
