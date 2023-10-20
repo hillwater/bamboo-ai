f154cd7b2721e8d0a4cc95c32998356a174c5b00
hillwater <hillwater.zju@gmail.com>
2023-10-20 11:13:20 +0800

    oss cp force overwrite

--- backupRedis.sh
+++ backupRedis.sh
@@ -5,4 +5,4 @@ docker cp redis:/data/dump.rdb .
 
 # save to aliyun OSS
 currentDay=`date '+%F'`
-ossutil cp dump.rdb oss://gomoku2/redis-backup/$currentDay/
+ossutil cp -f dump.rdb oss://gomoku2/redis-backup/$currentDay/
