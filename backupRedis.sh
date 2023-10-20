0145f1f9466f590cd50614859f3f02be4a70301b
hillwater <hillwater.zju@gmail.com>
2023-10-20 11:16:05 +0800

    save redis dump in /tmp

--- backupRedis.sh
+++ backupRedis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # copy from redis container to host
-docker cp redis:/data/dump.rdb .
+docker cp redis:/data/dump.rdb /tmp/
 
 # save to aliyun OSS
 currentDay=`date '+%F'`
-ossutil cp -f dump.rdb oss://gomoku2/redis-backup/$currentDay/
+ossutil cp -f /tmp/dump.rdb oss://gomoku2/redis-backup/$currentDay/
 echo "backup redis done: $currentDay"
