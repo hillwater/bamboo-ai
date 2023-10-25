c0a944db141565b72636d91de63071fc0995160b
hillwater <hillwater.zju@gmail.com>
2023-10-25 14:10:53 +0800

    update redis backup script

--- backupRedis.sh
+++ backupRedis.sh
@@ -11,4 +11,4 @@ docker cp redis:/data/dump.rdb /tmp/
 # save to aliyun OSS
 currentDay=`date '+%F'`
 ossutil cp -f /tmp/dump.rdb oss://gomoku2/redis-backup/$currentDay/
-echo "backup redis done: $currentDay"
+echo "backup redis done: `date`"
