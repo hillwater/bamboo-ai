6ce243f4033d0c50984f8d6a34f56211c1dbcefd
hillwater <hillwater.zju@gmail.com>
2023-10-25 13:01:28 +0800

    fix bug

--- runDocker.sh
+++ runDocker.sh
@@ -5,4 +5,4 @@ docker stop gomoku-web3
 docker rm gomoku-web3
 
 HOST=172.17.0.1
-docker run -d -e REDIS_HOST=$HOST  -p 80:3000 --name gomoku-web2 hillwater/gomoku-web:3.0.0
+docker run -d -e REDIS_HOST=$HOST  -p 80:3000 --name gomoku-web3 hillwater/gomoku-web:3.0.0
